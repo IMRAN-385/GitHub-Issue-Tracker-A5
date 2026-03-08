6b4ae367f5cdb660fbf740f32975148084e8a7b3
IMRAN-385 <imranhimel385@gmail.com>
2026-03-08 19:22:13 +0600

    cardSectionFinished

---
+++ script.js
@@ -0,0 +1,326 @@
+let issue = [
+  {
+    id : 1,
+    title : "Fix Navigation Menu On Mobile Devices",
+    description : "The navigation menu doesn't collapse properly on mobile devices...",
+    status : "HIGH",
+    assignee :"#1 by john_doe",
+    date :"1/15/2024",
+    labels: [
+    { name: "BUG", color: "red" },
+    { name: "HELP WANTED", color: "orange" }
+    ],
+    icon: "./B13-A5-Github-Issue-Tracker/assets/Open-Status.png"
+
+  },
+  {
+    id : 2,
+    title : "Fix Navigation Menu On Mobile Devices",
+    description : "The navigation menu doesn't collapse properly on mobile devices...",
+    status :"MEDIUM" ,
+    assignee :"#1 by john_doe",
+    date :"1/15/2024",
+    labels: [
+    { name: "BUG", color: "red" },
+    { name: "HELP WANTED", color: "orange" }
+    ],
+    icon: "./B13-A5-Github-Issue-Tracker/assets/Open-Status.png"
+  },
+  {
+    id : 3,
+    title : "Fix Navigation Menu On Mobile Devices",
+    description : "The navigation menu doesn't collapse properly on mobile devices...",
+    status :"LOW",
+    assignee :"#1 by john_doe",
+    date :"1/15/2024",
+    labels: [
+    { name: "BUG", color: "red" },
+    { name: "HELP WANTED", color: "orange" }
+    ],
+    icon: "./B13-A5-Github-Issue-Tracker/assets/Open-Status.png"
+  },
+  {
+    id : 4,
+    title : "Fix Navigation Menu On Mobile Devices",
+    description : "The navigation menu doesn't collapse properly on mobile devices...",
+    status :"HIGH",
+    assignee :"#1 by john_doe",
+    date :"1/15/2024",
+    labels: [
+    { name: "BUG", color: "red" },
+    { name: "HELP WANTED", color: "orange" }
+    ],
+    icon: "./B13-A5-Github-Issue-Tracker/assets/Open-Status.png"
+  },
+  {
+    id :5 ,
+    title : "Fix Navigation Menu On Mobile Devices",
+    description : "The navigation menu doesn't collapse properly on mobile devices...",
+    status :"HIGH",
+    assignee :"#1 by john_doe",
+    date :"1/15/2024",
+    labels: [
+    { name: "BUG", color: "red" },
+    { name: "HELP WANTED", color: "orange" }
+    ],
+    icon: "./B13-A5-Github-Issue-Tracker/assets/Open-Status.png"
+  },
+  {
+    id :6,
+    title : "Fix Navigation Menu On Mobile Devices",
+    description : "The navigation menu doesn't collapse properly on mobile devices...",
+    status :"LOW",
+    assignee :"#1 by john_doe",
+    date :"1/15/2024",
+    labels: [
+    { name: "ENHANCEMENT", color: "green" },
+    ],
+    icon: "./B13-A5-Github-Issue-Tracker/assets/Open-Status.png"
+  },
+  {
+    id : 7,
+    title : "Fix Navigation Menu On Mobile Devices",
+    description : "The navigation menu doesn't collapse properly on mobile devices...",
+    status :"MEDIUM" ,
+    assignee :"#1 by john_doe",
+    date :"1/15/2024",
+    labels: [
+    { name: "BUG", color: "red" },
+    { name: "HELP WANTED", color: "orange" }
+    ],
+    icon: "./B13-A5-Github-Issue-Tracker/assets/Open-Status.png"
+  },
+  {
+    id :8,
+    title : "Fix Navigation Menu On Mobile Devices",
+    description : "The navigation menu doesn't collapse properly on mobile devices...",
+    status :"HIGH",
+    assignee :"#1 by john_doe",
+    date :"1/15/2024",
+    labels: [
+    { name: "BUG", color: "red" },
+    { name: "HELP WANTED", color: "orange" }
+    ],
+    icon: "./B13-A5-Github-Issue-Tracker/assets/Open-Status.png"
+  },
+  {
+    id :9,
+    title : "Fix Navigation Menu On Mobile Devices",
+    description : "The navigation menu doesn't collapse properly on mobile devices...",
+    status :"HIGH",
+    assignee :"#1 by john_doe",
+    date :"1/15/2024",
+    labels: [
+    { name: "BUG", color: "red" },
+    { name: "HELP WANTED", color: "orange" }
+    ],
+    icon: "./B13-A5-Github-Issue-Tracker/assets/Open-Status.png"
+  },
+  {
+    id :10,
+    title : "Fix Navigation Menu On Mobile Devices",
+    description : "The navigation menu doesn't collapse properly on mobile devices...",
+    status :"MEDIUM",
+    assignee :"#1 by john_doe",
+    date :"1/15/2024",
+    labels: [
+    { name: "BUG", color: "red" },
+    { name: "HELP WANTED", color: "orange" }
+    ],
+    icon: "./B13-A5-Github-Issue-Tracker/assets/Open-Status.png"
+  },
+  {
+    id :11,
+    title : "Fix Navigation Menu On Mobile Devices",
+    description : "The navigation menu doesn't collapse properly on mobile devices...",
+    status :"LOW",
+    assignee :"#1 by john_doe",
+    date :"1/15/2024",
+    labels: [
+    { name: "BUG", color: "red" },
+    { name: "HELP WANTED", color: "orange" }
+    ],
+    icon: "./B13-A5-Github-Issue-Tracker/assets/Open-Status.png"
+  },
+  {
+    id :12,
+    title : "Fix Navigation Menu On Mobile Devices",
+    description : "The navigation menu doesn't collapse properly on mobile devices...",
+    status :"HIGH",
+    assignee :"#1 by john_doe",
+    date :"1/15/2024",
+    labels: [
+    { name: "BUG", color: "red" },
+    { name: "HELP WANTED", color: "orange" }
+    ],
+    icon: "./B13-A5-Github-Issue-Tracker/assets/Open-Status.png"
+  },
+  {
+    id :13,
+    title : "Fix Navigation Menu On Mobile Devices",
+    description : "The navigation menu doesn't collapse properly on mobile devices...",
+    status :"HIGH",
+    assignee :"#1 by john_doe",
+    date :"1/15/2024",
+    labels: [
+    { name: "BUG", color: "red" },
+    { name: "HELP WANTED", color: "orange" }
+    ],
+    icon: "./B13-A5-Github-Issue-Tracker/assets/Open-Status.png"
+  },
+  {
+    id :14,
+    title : "Fix Navigation Menu On Mobile Devices",
+    description : "The navigation menu doesn't collapse properly on mobile devices...",
+    status :"MEDIUM",
+    assignee :"#1 by john_doe",
+    date :"1/15/2024",
+    labels: [
+    { name: "BUG", color: "red" },
+    { name: "HELP WANTED", color: "orange" }
+    ],
+    icon: "./B13-A5-Github-Issue-Tracker/assets/Open-Status.png"
+  },
+  {
+    id :15 ,
+    title : "Fix Navigation Menu On Mobile Devices",
+    description : "The navigation menu doesn't collapse properly on mobile devices...",
+    status :"LOW",
+    assignee :"#1 by john_doe",
+    date :"1/15/2024",
+    labels: [
+    { name: "BUG", color: "red" },
+    { name: "HELP WANTED", color: "orange" }
+    ],
+    icon: "./B13-A5-Github-Issue-Tracker/assets/Open-Status.png"
+  },
+  {
+    id :16,
+    title : "Fix Navigation Menu On Mobile Devices",
+    description : "The navigation menu doesn't collapse properly on mobile devices...",
+    status :"HIGH",
+    assignee :"#1 by john_doe",
+    date :"1/15/2024",
+    labels: [
+    { name: "BUG", color: "red" },
+    { name: "HELP WANTED", color: "orange" }
+    ],
+    icon: "./B13-A5-Github-Issue-Tracker/assets/Open-Status.png"
+  },
+];
+let currentFilter ="all";
+
+const totalNumber = document.getElementById("totalnumbers");
+const open = document.getElementById("opened");
+const close = document.getElementById("closed");
+const allCardsSection = document.querySelector
+(".allcard");
+
+function init(){
+  renderJobs();
+  updateCounts();
+}
+
+
+function updateCounts() {
+  const allissue = issue.filter(a =>a.status=== "HIGH");
+  const openBtn = issue.filter(a =>a.status=== "MEDIUM");
+  const closedBtn = issue.filter(a =>a.status=== "LOW");
+  const totalNumber = document.getElementById("totalnumbers");
+  const allB = document.getElementById("all")
+  const open = document.getElementById("opened");
+  const close = document.getElementById("closed");
+  const count = document.querySelector(".num")
+
+  // if (allB) allB.innerText = allissue.length;
+  // if (close) close.innerText = closedBtn.length;
+  // if (open) open.innerText = openBtn.length;
+
+  if(count){
+    let score = 0;
+    if (currentFilter === "all") score = issue.length;
+    else if (currentFilter === "opened") score = openBtn.length;
+    else if (currentFilter === "closed") score = closedBtn.length;
+    count.innerText = score;
+  }
+  
+}
+
+function renderJobs (){
+  allCardsSection.innerHTML = "";
+
+
+  
+
+
+  const filteredIssue = issue.filter(issue => {
+    if  (currentFilter === "all" ) return true;
+    return issue.status=== currentFilter.toUpperCase();
+  });
+
+  if(filteredIssue.length === 0){
+    allCardsSection.innerHTML = `<div class="text-center py-20 text-gray-400">No issues found.</div>`;
+    return;
+  }
+  filteredIssue.forEach(issue => {
+      const section = document.createElement("section");
+
+      let color ="red";
+      let borderColor = "green"
+
+  if(issue.status==="HIGH") {
+    color = "red";
+    borderColor = "green";
+  }
+  else if (issue.status==="MEDIUM") {
+    color =  "yellow";
+    borderColor= "green";
+  }
+  else if (issue.status==="LOW"){
+    color ="gray";
+    borderColor="purple";
+  } 
+
+  let icon="";
+
+  if (issue.status === "HIGH"){
+    icon = "./B13-A5-Github-Issue-Tracker/assets/Open-Status.png";
+  }
+  else if (issue.status === "MEDIUM"){
+    icon = "./B13-A5-Github-Issue-Tracker/assets/Open-Status.png";
+  }
+  else if (issue.status === "LOW"){
+    icon = "./B13-A5-Github-Issue-Tracker/assets/Closed- Status .png";
+  }
+
+
+
+    section.className = "mb-10";
+    let statusClass = "btn-soft btn-primary";
+    if (issue.status === "OPENED") statusClass = "btn-success text-white";
+    section.innerHTML =`
+    <div class="w-70  border-t-5 border-${borderColor}-600 rounded-lg shadow-2xl sm:ml-20 lg:m-auto ">
+      <div class="flex justify-between mt-4 mr-3 "><img class=" ml-5 h-8 w-8" src="${icon}">
+        <p class="px-6 py-1  rounded-2xl text-${color}-600 bg-${color}-200 border-2  border-${color}-400">${issue.status}</p>
+
+      </div>
+      <div>
+        <h2 class="font-bold text-xl px-5">${issue.title}</h2>
+        <p class="text-gray-400 px-5">(${issue.description})</p>
+      </div>
+      <div class="px-5 py-4 border-b-2 border-gray-300">
+        ${issue.labels.map(label => 
+  `<button class="px-3.5 rounded-xl text-${label.color}-600 bg-${label.color}-200 border-2 border-${label.color}-400 mr-2">
+     ${label.name}
+   </button>`).join(' ')}
+      </div>
+      <div class="p-5  text-gray-500">
+        <p class="mb-2">${issue.assignee}</p>
+        <p>${issue.date}</p>
+      </div>
+    </div>
+    `;
+    allCardsSection.appendChild(section);
+  });
+}
+init();
